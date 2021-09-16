599f810b248ef9833efc994fff46bdb7d901a6c7
Xan Johnson <xanjohns@gmail.com>
2021-09-16 15:48:21 -0600

    Include python heredoc

--- add_subtree.sh
+++ add_subtree.sh
@@ -24,7 +24,17 @@ else
   kill -INT $$
   fi
   echo "Clone repos"
-  curl -s https://api.github.com/orgs/SymbiFlow/repos?per_page=200 | python ../merger_help.py
+  python <<EOF 
+import urllib.request
+import json;
+import os;
+with urllib.request.urlopen('https://api.github.com/orgs/SymbiFlow/repos?per_page=200') as response:
+  json_arr = json.loads(response.read())
+  for val in json_arr:
+    if val['fork'] == False:
+      url = val['clone_url']
+      os.system("gh repo fork {} --clone".format(url))
+EOF
 fi
 
 
